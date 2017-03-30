9545a36bdd3063825efbc69b38f85fdab1c7299c
a-j-welsh <a.welsh.p@outlook.com>
2017-03-29 22:02:11 -0700

    Concentric circles
    The shape is different from what we planned but its the best I can do
    at this point. I need to adjust to that it cycles through the colours
    but this is what we get to work with

--- assignment_03_B_C/generate_circle.py
+++ assignment_03_B_C/generate_circle.py
@@ -1,7 +1,7 @@
 import sys
 import math
 import Line_Point_colour
-
+# pylint: disable=W0312
 
 '''
 purpose
@@ -12,70 +12,30 @@ preconditions
     c is a valid colour string from css_colour.txt
 '''
 
-def draw_circle(circletop,colour,sides,count):
-	if count == 4:
-		central_angle = 2 * math.pi / sides
-		new_circle = Line_Point_colour.Point(circletop.x,circletop.y)
-		new_circle.translate(circletop.x,circletop.y)
-		while sides > 0:
-			new_c1 = Line_Point_colour.Point(new_circle.x, new_circle.y)
-			new_c1.rotate(central_angle)
-			print 'line', Line_Point_colour.Line(new_circle, new_c1)
-			new_circle = new_c1
-			sides = sides - 1
-	if count == 3:
-		central_angle = 2 * math.pi / sides
-		new_circle = Line_Point_colour.Point(circletop.x,circletop.y)
-		new_circle.translate(circletop.x,float(circletop.y-circletop.y/2))
-		while sides > 0:
-			new_c1 = Line_Point_colour.Point(new_circle.x, new_circle.y)
-			new_c1.rotate(central_angle)
-			print 'line', Line_Point_colour.Line(new_circle, new_c1)
-			new_circle = new_c1
-			sides = sides - 1
-	if count == 2:
-		central_angle = 2 * math.pi / sides
-		new_circle = Line_Point_colour.Point(circletop.x,circletop.y)
-		new_circle.translate(float(-(circletop.y/4)),float(circletop.y*(3/4)))
-		while sides > 0:
-			new_c1 = Line_Point_colour.Point(new_circle.x, new_circle.y)
-			new_c1.rotate(central_angle)
-			print 'line', Line_Point_colour.Line(new_circle, new_c1)
-			new_circle = new_c1
-			sides = sides - 1
-	if count == 1:
-		central_angle = 2 * math.pi / sides
-		new_circle = Line_Point_colour.Point(circletop.x,circletop.y)
-		new_circle.translate(float(circletop.y/4),float(circletop.y*(3/4)))
-		while sides > 0:
-			new_c1 = Line_Point_colour.Point(new_circle.x, new_circle.y)
-			new_c1.rotate(central_angle)
-			print 'line', Line_Point_colour.Line(new_circle, new_c1)
-			new_circle = new_c1
-			sides = sides - 1
-	else:
+def draw_circle(circletop, layer, spacing, cr, sides):
+	if sides == 0:
 		return
+	sides = sides*(1-layer*1/10)
+	central_angle = 2 * math.pi / sides
+	new_x0 = float(0)
+	new_y0 = float(248-spacing*layer)
+	if new_y0 == 0:
+		new_y0 = float(248)
+	new_circle = Line_Point_colour.Point(new_x0, new_y0)
+	while sides > 0:
+		new_c1 = Line_Point_colour.Point(new_circle.x, new_circle.y)
+		new_c1.rotate(central_angle)
+		print 'line', Line_Point_colour.Line(new_circle, new_c1, cr)
+		new_circle = new_c1
+		sides = sides - 1
+	return new_circle
 
-
-def recursive_draw(circletop, layer, colour, sides):
+def recursive_draw(circletop, layer, spacing, cr, sides):
 	# end recursion if base case reached
-	if layer == 0 or sides==0:
+	if layer == 0 or sides == 0:
 		return
-
-	# print circletop
-	print 'line', circletop
-
-	count = 4
-	sides = sides/2
-
 	# continue with recursion
-	recursive_draw(draw_circle(circletop/2,colour,sides,count),layer-1,colour,sides)
-	'''
-	recursive_draw( draw_circle(circletop/2,colour,s/2,count-1), l-1,colour,s/2)
-	recursive_draw( draw_circle(circletop/2,colour,s/2,count-2), l-1,colour,s/2)
-	recursive_draw( draw_circle(circletop/2,colour,s/2,count-3), l-1,colour,s/2)
-	'''
-
+	recursive_draw(draw_circle(circletop, layer, spacing, cr, sides), layer-1, spacing, cr, sides)
 # ********** process the command line arguments
 
 if len(sys.argv) != 4:
@@ -92,17 +52,10 @@ if layers < 1 or colourRange > layers:
 	print >> sys.stderr, 'Syntax: ' + sys.argv[0] + ' layers colourRange '
 	sys.exit(3)
 
-# starting point: x-0 y-176 , at top of canvas
-s = 2000
+# starting point: x- 0 y- 248 , at top of canvas
 x0 = float(0)
 y0 = float(248)
-central_angle = 2 * math.pi / s
-p0 = Line_Point_colour.Point(x0,y0)
-while s > 0:
-	p1 = Line_Point_colour.Point(p0.x, p0.y)
-	p1.rotate(central_angle)
-	print 'line', Line_Point_colour.Line(p0, p1,colour)
-	p0 = p1
-	s = s - 1
-
-recursive_draw(p0, layers,colour,s)
+s = 2000
+spacing = 248/layers
+circletop = Line_Point_colour.Point(x0, y0)
+recursive_draw(circletop, layers, spacing, colour, s)
